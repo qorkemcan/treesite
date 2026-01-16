3c81560068ac8fbe7252566405c3630a542aa8db
qorkemcan <gorkemcan@aol.com>
2026-01-16 16:19:13 +0300

    Sitemap URL eklendi
    Sitemap URL eklendi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Sitemizin tam adresi (Site haritası için şart!)
+  site: 'https://protreetrim.com',
+  integrations: [sitemap()],
+});
