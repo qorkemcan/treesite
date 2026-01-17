cba5777e5b2e69cf75f2d24654d9552a59f367af
qorkemcan <gorkemcan@aol.com>
2026-01-17 17:19:26 +0300

    dfg

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// src/config.js
+export const SITE_CONFIG = {
+  brandName: "ProTreeTrim™ Florida",
+  phone: "(800) 123-4567", // Marketcall numaran gelince burayı değiştir
+  phoneHref: "tel:+18001234567",
+  email: "gorkemcan@aol.com",
+  year: "2026",
+  siteUrl: "https://www.protreetrim.com"
+};
