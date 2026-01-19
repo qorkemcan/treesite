5564a2e5f70f1dd9977b5f029f7a5e188c362037
qorkemcan <gorkemcan@aol.com>
2026-01-19 18:00:37 +0300

    w

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // src/config.js
 export const SITE_CONFIG = {
   brandName: "ProTreeTrim™ Florida",
-  phone: "(800) 123-4567", // Marketcall numaran gelince burayı değiştir
-  phoneHref: "tel:+18001234567",
+  phone: "(855) 498-2578", // Marketcall numaran gelince burayı değiştir
+  phoneHref: "tel:+18554982578",
   email: "desk@contractor.net", // Güncellendi!
   year: "2026",
   siteUrl: "https://www.protreetrim.com",
